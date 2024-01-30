54838759f1b5c83e8df264a6705dd92705754663
Aditya <4adi26@gmail.com>
2024-01-30 21:22:31 +0530

    Created features section

---
+++ client/src/sections/Features.jsx
@@ -0,0 +1,44 @@
+import { HandCoins, ShieldCheck, Rocket } from "lucide-react"
+
+const Features = () => {
+  const features = [
+    {
+      icon: <HandCoins />,
+      title: "Save on travel costs",
+      desc: "Share the cost of your journey with other passengers, significantly reducing your travel expenses compared to solo travelling."
+    },
+    {
+      icon: <ShieldCheck />,
+      title: "Join a trustworthy community",
+      desc: "Connect with like-minded people. Meet new people who share your interest and destination through carpooling."
+    },
+    {
+      icon: <Rocket />,
+      title: "Carpooling made simple",
+      desc: "Find rides with ease. Search for rides that match your destination, schedule, and preference with just a few taps."
+    }
+  ]
+  return (
+    <section className="text-foreground container max-w-screen-xl">
+      <div className="md:px-5 py-16 mx-auto">
+        <div className="flex flex-wrap">
+          {features.map(f => 
+            <div className="md:w-1/3 flex rounded-lg h-full p-6 flex-col" key={f.title}>
+              <div className="flex items-center mb-3">
+                <div className="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-full bg-primary-foreground text-primary flex-shrink-0">
+                  {f.icon}
+                </div>
+                <h2 className="text-lg">{f.title}</h2>
+              </div>
+              <div className="flex-grow text-muted-foreground">
+                <p className="leading-relaxed text-base">{f.desc}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Features
