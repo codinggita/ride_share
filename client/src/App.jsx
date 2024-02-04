f3d5fd8178f0ab3d570769a51b29363c1a319603
Aditya <4adi26@gmail.com>
2024-02-04 20:09:31 +0530

    Added ride detail page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import OfferSeat from './pages/OfferSeat'
 import Footer from './components/Footer'
 import SearchPage from './pages/SearchPage'
 import Error from './pages/Error'
+import RideDetail from './pages/RideDetail'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
       <Route path="/*" element={<Error />} />
       <Route path="/search" element={<SearchPage />} />
       <Route path="/offer-seat" element={<OfferSeat />} />
+      <Route path="/ride/:rideId" element={<RideDetail />} />
     </Routes>
     <Footer />
     </>
