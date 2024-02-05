6f34c76997c01681c82f0feecc4753b42e5501e0
Aditya <4adi26@gmail.com>
2024-02-05 11:00:52 +0530

    added cors middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import cors from "cors"
 
 import authRoute from "./routes/auth.routes.js"
 import userRoute from "./routes/user.routes.js"
@@ -21,6 +22,7 @@ const connectDB = (url) => {
 };
 
 //middlewares
+app.use(cors())
 app.use(express.json())
 
 app.use("/api/users", userRoute);
