613417e271ac9028d0c802c6e8bfe7faef61d677
Aditya <4adi26@gmail.com>
2024-02-12 07:50:06 +0530

    fix logout

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,28 @@
 import logo from "../assets/logo.svg"
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useNavigate,  } from "react-router-dom"
 import { Avatar, AvatarFallback, AvatarImage  } from "./ui/avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Search, PlusCircle, LogOut, User } from "lucide-react";
 import LoginSignupDialog from "./LoginSignupDialog";
 import { AuthContext } from "@/context/AuthContext";
 import { useContext } from "react";
+import axios from "axios";
+const apiUri = import.meta.env.VITE_REACT_API_URI
+
 
 const Header = () => {
-  const {user} = useContext(AuthContext)
+  const {user, dispatch} = useContext(AuthContext)
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try{
+      await axios.get(`${apiUri}/auth/logout`, {withCredentials: true});
+      dispatch({ type: 'LOGOUT' });
+      navigate("/");
+    }catch(err){
+      console.log(err)
+    }
+  };
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background mx-auto flex p-3 lg:px-16 items-center justify-end">
@@ -26,8 +40,8 @@ const Header = () => {
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar>
-              <AvatarImage src={user.profilePicture || "https://github.com/shadcn.png"} />
-              <AvatarFallback>Profile</AvatarFallback>
+              <AvatarImage src={user.user.profilePicture} />
+              <AvatarFallback className="select-none text-primary text-xl font-bold">{user.user?.name[0]}</AvatarFallback>
             </Avatar>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
@@ -38,7 +52,7 @@ const Header = () => {
                 <User className="mr-2 h-4 w-4"/><span>Profile</span>
               </Link>
               </DropdownMenuItem>
-            <DropdownMenuItem><LogOut className="mr-2 h-4 w-4"/><span>Log Out</span></DropdownMenuItem>
+            <DropdownMenuItem className='cursor-pointer' onClick={handleLogout}><LogOut className="mr-2 h-4 w-4"/><span>Log Out</span></DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       }
